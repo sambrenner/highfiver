d6a635b162e9c7177b2c7f8193a54bad034a9e85
Sam Brenner <samjbrenner@gmail.com>
2013-11-03 15:04:51 -0500

    beginning leaderboard

---
+++ public/javascripts/highfiver/highfiver.leaderboard.js
@@ -0,0 +1,15 @@
+var highfiver = highfiver || {};
+
+highfiver.leaderboard = (function(window,document) {
+  var self = {
+    init: function() {
+
+    }
+  };
+
+  return self;
+})(this,this.document);
+
+$(document).ready(function() {
+  highfiver.leaderboard.init();
+})
---
+++ routes/leaderboard.js
@@ -0,0 +1,3 @@
+exports.leaderboard = function(req,res) {
+
+};
